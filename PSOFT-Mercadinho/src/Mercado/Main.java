a7483cadbd87fb4617b538800d741992cf9badcc
Arthurnevs <jose.arthur.brito@ccc.ufcg.edu.br>
2022-05-10 10:45:51 -0300

    refatorando

--- PSOFT-Mercadinho/src/Mercado/Main.java
+++ PSOFT-Mercadinho/src/Mercado/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Produto produto = new Produto("Leite", "Leite de caixinha");
+		Produto produto = new Produto("Leite");
 		Lote lote = new Lote(produto,15);
 		
 	}
