e66c739eafb9c2de5610e9a56a82d076a542196e
Arthurnevs <jose.arthur.brito@ccc.ufcg.edu.br>
2022-05-10 10:41:23 -0300

    Segundo commit

--- PSOFT-Mercadinho/src/Mercado/Produto.java
+++ PSOFT-Mercadinho/src/Mercado/Produto.java
@@ -4,17 +4,22 @@ public class Produto {
 
 	private String id;
 	private String nome;
+	private String descricao;
 	
-	public Produto(String nome) {
+	public Produto(String nome, String descricao) {
 		super();
 		this.nome = nome;
+		this.descricao = descricao;
 	}
 
 	@Override
 	public String toString() {
-		return "Produto [nome=" + nome + "]";
+		return "Produto [nome=" + nome + ", descricao=" + descricao + "]";
 	}
 	
+	
+	
+
 	
 	
 	
